ebca9fee25258fa4e7316fbe5c7aa2845954a8fd
Michael Dominic K <mdk@new-wild.com>
2009-08-04 01:15:57 +0200

    Adding specs.

--- spec/scrap-test.rb
+++ spec/scrap-test.rb
@@ -1,43 +1,6 @@
 require 'rubygems'
 require 'scrap'
 require 'spec'
-require 'storage'
-
-describe 'Storage' do
-  before(:each) do
-    @db = TDB::new
-    @db.open('test.tct', TDB::OWRITER | TDB::OCREAT)
-    @db.vanish
-    @st = Storage::new(@db)
-  end
-
-  it 'can be created' do
-    @st.should_not be_nil
-  end
-
-  it 'can save a non-saved scrap' do
-    scrap = mock('Scrap')
-    scrap.stub!(:saved?).and_return(false)
-    scrap.stub!(:to_db_keys).and_return({'content' => 'Content', 'something' => 'Something'})
-    scrap.should_receive(:local_id=).once
-    @st << scrap
-  end
-
-  it 'can save a saved scrap' do
-    scrap = mock('Scrap')
-    scrap.stub!(:saved?).and_return(true)
-    scrap.stub!(:local_id).and_return(666)
-    scrap.stub!(:to_db_keys).and_return({'content' => 'Content'})
-    scrap.should_not_receive(:local_id=)
-    @st << scrap
-    scrap.local_id.should == 666
-    @db.get('666')['content'].should == 'Content'
-  end
-
-  after(:each) do
-    @st.close
-  end
-end
 
 describe 'Scrap' do
 
@@ -129,5 +92,4 @@ describe 'Scrap' do
     @s.metadata['Title'].should == 'Hello'
     @s.metadata['Another'].should == 'Another'
   end
-
 end
