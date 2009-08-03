10a93bf694c642ae47d1db5bf8aada5f9deca2f3
Michael Dominic K <mdk@new-wild.com>
2009-08-04 01:27:33 +0200

    Adding context support to specs.

--- spec/scrap-test.rb
+++ spec/scrap-test.rb
@@ -8,88 +8,94 @@ describe 'Scrap' do
     @s = Scrap::new
   end
 
-  it "can be created" do
-    @s.should_not be_nil
-  end
+  context 'scrap basic' do
 
-  it "has a settable content" do
-    @s.content = "This is content"
-    @s.content.should == "This is content"
-  end
+    it "can be created" do
+      @s.should_not be_nil
+    end
 
-  it "has a settable metadata" do
-    @s.metadata['Title'] = "TitleOne"
-    @s.metadata['Nick'] = "nick"
-    @s.metadata['Title'].should == "TitleOne"
-    @s.metadata['Nick'].should == "nick"
-  end
+    it "has a settable content" do
+      @s.content = "This is content"
+      @s.content.should == "This is content"
+    end
 
-  it 'is fresh by default' do
-    @s.fresh?.should be_true
-  end
+    it "has a settable metadata" do
+      @s.metadata['Title'] = "TitleOne"
+      @s.metadata['Nick'] = "nick"
+      @s.metadata['Title'].should == "TitleOne"
+      @s.metadata['Nick'].should == "nick"
+    end
 
-  it 'is not saved by default' do
-    @s.saved?.should_not be_true
-  end
+    it 'is fresh by default' do
+      @s.fresh?.should be_true
+    end
 
-  it "can be serialized to keys" do
-    @s.content = "Hello"
-    @s.remote_id = 'AAA'
-    @s.revision = 3
-    @s.metadata['Title'] = "TitleOne"
-    @s.metadata['Nick'] = "nick"
+    it 'is not saved by default' do
+      @s.saved?.should_not be_true
+    end
+
+    it "can be serialized to keys" do
+      @s.content = "Hello"
+      @s.remote_id = 'AAA'
+      @s.revision = 3
+      @s.metadata['Title'] = "TitleOne"
+      @s.metadata['Nick'] = "nick"
     
-    data = @s.to_db_keys
+      data = @s.to_db_keys
 
-    data.should_not be_nil
-    data['content'].should == "Hello"
-    data['remote_id'].should == "AAA"
-    data['revision'].should == "3"
-    data['metadata:Title'].should == 'TitleOne'
-    data['metadata:Nick'].should == 'nick'
+      data.should_not be_nil
+      data['content'].should == "Hello"
+      data['remote_id'].should == "AAA"
+      data['revision'].should == "3"
+      data['metadata:Title'].should == 'TitleOne'
+      data['metadata:Nick'].should == 'nick'
+    end
   end
 
-  it 'truly serializes to strings' do
-    @s.revision = 3
+  context 'scrap serialization' do
+
+    it 'truly serializes to strings' do
+      @s.revision = 3
     
-    data = @s.to_db_keys
+      data = @s.to_db_keys
     
-    data.should_not be_nil
-    data['revision'].should_not == 3
-  end
+      data.should_not be_nil
+      data['revision'].should_not == 3
+    end
 
-  it "when serialized, nil is not put" do
-    data = @s.to_db_keys
-    data['remote_id'].should == ''
-    data['content'].should == ''
-    data['revision'].should == '1'
-  end
+    it "when serialized, nil is not put" do
+      data = @s.to_db_keys
+      data['remote_id'].should == ''
+      data['content'].should == ''
+      data['revision'].should == '1'
+    end
 
-  it 'can unserialize from db keys' do
-    data = { 'remote_id' => 'BBB', 
-             'revision' => '67', 
-             'content' => 'HelloWorld' }
-    @s.from_db_keys('567', data)
+    it 'can unserialize from db keys' do
+      data = { 'remote_id' => 'BBB', 
+               'revision' => '67', 
+               'content' => 'HelloWorld' }
+      @s.from_db_keys('567', data)
   
-    @s.local_id.should == '567'
-    @s.remote_id.should == 'BBB'
-    @s.revision.should == 67
-    @s.content.should == 'HelloWorld'
-  end
+      @s.local_id.should == '567'
+      @s.remote_id.should == 'BBB'
+      @s.revision.should == 67
+      @s.content.should == 'HelloWorld'
+    end
 
-  it 'can unserialize metadata too' do
-   data = { 'remote_id' => 'CCC', 
-            'revision' => '87', 
-            'content' => 'HelloWorld', 
-            'metadata:Title' => 'Hello', 
-            'metadata:Another' => 'Another' }
-    @s.from_db_keys('666', data)
+    it 'can unserialize metadata too' do
+      data = { 'remote_id' => 'CCC', 
+               'revision' => '87', 
+               'content' => 'HelloWorld', 
+               'metadata:Title' => 'Hello', 
+               'metadata:Another' => 'Another' }
+      @s.from_db_keys('666', data)
     
-    @s.local_id.should == '666'
-    @s.remote_id.should == 'CCC'
-    @s.revision.should == 87
-    @s.content.should == 'HelloWorld'
-    @s.metadata['Title'].should == 'Hello'
-    @s.metadata['Another'].should == 'Another'
+      @s.local_id.should == '666'
+      @s.remote_id.should == 'CCC'
+      @s.revision.should == 87
+      @s.content.should == 'HelloWorld'
+      @s.metadata['Title'].should == 'Hello'
+      @s.metadata['Another'].should == 'Another'
+    end
   end
 end
--- spec/storage-test.rb
+++ spec/storage-test.rb
@@ -14,27 +14,30 @@ describe 'Storage' do
     @st = Storage::new(@db)
   end
 
-  it 'can be created' do
-    @st.should_not be_nil
-  end
+  context 'storage basic' do
 
-  it 'can save a non-saved scrap' do
-    scrap = mock('Scrap')
-    scrap.stub!(:saved?).and_return(false)
-    scrap.stub!(:to_db_keys).and_return({'content' => 'Content', 'something' => 'Something'})
-    scrap.should_receive(:local_id=).once
-    @st << scrap
-  end
+    it 'can be created' do
+      @st.should_not be_nil
+    end
 
-  it 'can save a saved scrap' do
-    scrap = mock('Scrap')
-    scrap.stub!(:saved?).and_return(true)
-    scrap.stub!(:local_id).and_return(666)
-    scrap.stub!(:to_db_keys).and_return({'content' => 'Content'})
-    scrap.should_not_receive(:local_id=)
-    @st << scrap
-    scrap.local_id.should == 666
-    @db.get('666')['content'].should == 'Content'
+    it 'can save a non-saved scrap' do
+      scrap = mock('Scrap')
+      scrap.stub!(:saved?).and_return(false)
+      scrap.stub!(:to_db_keys).and_return({'content' => 'Content', 'something' => 'Something'})
+      scrap.should_receive(:local_id=).once
+      @st << scrap
+    end
+
+    it 'can save a saved scrap' do
+      scrap = mock('Scrap')
+      scrap.stub!(:saved?).and_return(true)
+      scrap.stub!(:local_id).and_return(666)
+      scrap.stub!(:to_db_keys).and_return({'content' => 'Content'})
+      scrap.should_not_receive(:local_id=)
+      @st << scrap
+      scrap.local_id.should == 666
+      @db.get('666')['content'].should == 'Content'
+    end
   end
 
   after(:each) do
