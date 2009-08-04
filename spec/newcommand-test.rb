7ccd383309b151b9894609a8b8fbcfb91e92d133
Michael Dominic K <mdk@new-wild.com>
2009-08-04 11:26:18 +0200

    Test the new command.

--- spec/newcommand-test.rb
+++ spec/newcommand-test.rb
@@ -1,30 +1,30 @@
 require 'rubygems'
-require 'optwalkers'
+require 'newcommand'
 require 'spec'
 
-describe 'NewWalker' do
-    context 'basic new walker' do
+describe 'NewCommand' do
+    context 'basic new command' do
       it 'can be created' do
-        @w = NewWalker::new(['new'])
+        @w = NewCommand::new(['new'])
         @w.should_not be_nil
       end
 
-      it "can't be created with invalid action" do
+      it "can't be created with invalid word" do
         lambda {
-          NewWalker::new(['save'])
+          NewCommand::new(['save'])
         }.should raise_error
       end
     end
 
-    context 'walking new walker' do
+    context 'walking new command' do
       it 'can walk a set with title' do
-        @w = NewWalker::new(['new', 'some title'])
+        @w = NewCommand::new(['new', 'some title'])
         @w.walk!
         @w.title.should == 'some title'
       end
 
       it 'requires a valid title' do
-        @w = NewWalker::new(['new'])
+        @w = NewCommand::new(['new'])
         lambda { @w.walk! }.should raise_error
       end
     end
